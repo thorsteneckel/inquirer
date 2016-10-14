84fa36c0e75d3b52d372dcc89718483eb097c89b
Thorsten Eckel <te@znuny.com>
2016-10-14 12:59:14 +0200

    Added SimpleCov and CodeClimate.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,7 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'rspec'
 require 'inquirer'
+require 'simplecov'
 
 require 'inquirer/prompts/checkbox_examples'
 require 'inquirer/prompts/list_examples'
@@ -8,6 +10,30 @@ require 'inquirer/style/filterable_examples'
 require 'inquirer/style/input_examples'
 require 'inquirer/style/list_examples'
 require 'inquirer/style_examples'
+
+# Check for coverage stuffs
+formatters = []
+if ENV['CODECLIMATE_REPO_TOKEN']
+  require 'codeclimate-test-reporter'
+  formatters << CodeClimate::TestReporter::Formatter
+end
+
+if ENV['CODECOV_TOKEN']
+  require 'codecov'
+  formatters << SimpleCov::Formatter::Codecov
+end
+
+unless formatters.empty?
+  SimpleCov.formatters = formatters
+end
+
+SimpleCov.start do
+  # Don't get coverage on the test cases themselves.
+  add_filter '/spec/'
+  add_filter '/test/'
+  # Codecov doesn't automatically ignore vendored files.
+  add_filter '/vendor/'
+end
 
 module IOHelper
   extend self
